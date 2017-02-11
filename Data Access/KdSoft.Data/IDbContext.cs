94cd0a41811100c7403bb320b70b6b621fb954b1
Karl Waclawek <karl.waclawek@qlinesolutions.com>
2017-02-11 14:53:01 -0500

    [KdSoft.Data] Made IDbContext derive from IDisposable so that IDbContext references can be disposed.

--- Data Access/KdSoft.Data/IDbContext.cs
+++ Data Access/KdSoft.Data/IDbContext.cs
@@ -1,10 +1,11 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Data.Common;
 using System.Globalization;
 
 namespace KdSoft.Data
 {
-    public interface IDbContext
+    public interface IDbContext: IDisposable
     {
         DbConnection OpenConnection(string name);
 
