202a91b326d500da2cf4e522069636ffcef8e091
Karl Waclawek <karl.waclawek@qlinesolutions.com>
2017-07-01 00:57:18 -0400

    [Solution] Improved SuperClear.cmd to remove the bin, obj folders themselves.

--- Projects_SuperClear.cmd
+++ Projects_SuperClear.cmd
@@ -1,4 +1,4 @@
-for /d /r . %%d in (artifacts\*, bin\*, obj\*, debug\*, release\*) do @if exist "%%d" echo "%%d" && "tools\SuperDelete.exe" "%%d"
+for /d /r . %%d in (artifacts\*, bin, obj) do @if exist "%%d" echo "%%d" && "tools\SuperDelete.exe" "%%d"
 
 pause
 
